27f172e56d538fbe4137a27c93964a7d00c3119b
PhucNood <phucnvhd772000@gmail.com>
2022-01-20 11:48:42 +0700

    update

--- Day3/Clock_App/Clock.cs
+++ Day3/Clock_App/Clock.cs
@@ -12,9 +12,12 @@ namespace Clock_App
         public delegate void OnChangeSecondHandle();
         public event  OnChangeSecondHandle OnChangeSecondEvent;
 
-      
-        public Clock(){OnChangeSecondEvent+=new OnChangeSecondHandle(()=>{System.Console.WriteLine(DateTime.Now.ToString());});}
-
+       public void Fire(){
+           if(OnChangeSecondEvent!=null){
+                 OnChangeSecondEvent();              
+           }
+       }
+     
         
 
         
--- Day3/Clock_App/Program.cs
+++ Day3/Clock_App/Program.cs
@@ -6,19 +6,23 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Diagnostics;
 
-namespace Clock_App{
-    class Program{
-      
-      public static void Main(string[] args){
-          while (true)
-          {
-              Thread.Sleep(1000);
-                Clock clock = new Clock();
-                
+namespace Clock_App
+{
+    class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            while (true)
+            {
+
+                    Thread.Sleep(1000);
+                    Clock clock = new Clock();
+                    clock.OnChangeSecondEvent += () => { System.Console.WriteLine(DateTime.Now.ToString()); };
+                    clock.Fire();
                
-                
-          }
-      }
-       
+            }
+        }
+
     }
 }
